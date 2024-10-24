714b8dc56271dc06172359146e39e7cc0c0b7f6d
Nguyễn Thanh Tuấn <ila.thanhtuan@gmail.com>
2024-10-24 19:20:25 +0700

    Add a_star.py layout (haven't added heuristic method)

---
+++ algorithms/a_star.py
@@ -0,0 +1,14 @@
+from algorithms.base_search import BaseSearch
+from data_structures.priority_queue import PriorityQueue
+from core.grid import Grid
+from core.node import Node
+
+class DFS(BaseSearch):
+    def __init__(self, grid: Grid, next_node_data_structure: PriorityQueue = PriorityQueue()) -> None:
+        super().__init__(next_node_data_structure, grid)
+
+    def calculate_g(self, node) -> int:
+        return node.g_cost
+    
+    def calculate_h(self, node : Node) -> int:
+        return 0
