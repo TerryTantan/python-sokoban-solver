c96ff367943119eac2c51547455cf20bbfd59c57
Ta Chi Thanh Danh <22120049@student.hcmus.edu.vn>
2024-10-24 14:27:26 +0700

    Add IO Handler

---
+++ io/io_handler.py
@@ -0,0 +1,27 @@
+import os
+from ..core.grid import Grid
+
+
+class IOHandler:
+    """
+    A class to handle reading input for the Sokoban game.
+
+    Attributes:
+        input_path (str): The path to the input file.
+        output_path (str): The path to the output file.
+    """
+
+    def __init__(self, input_path: str, output_path: str):
+        self.input_path = input_path
+        self.output_path = output_path
+
+    def load_from_file(self) -> Grid:
+        with open(self.input_path, "r") as file:
+            weight_data = file.readline()  # no need to strip
+            grid_data = file.readlines()
+
+        return Grid(weight_data, grid_data)
+
+    def save_to_file(self) -> None:
+        with open(self.output_path, "w") as file:
+            file.write("Hello world!")
