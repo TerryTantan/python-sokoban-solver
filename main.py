2d201c87769aeaa2338c53905bc8f62fb9813400
Nguyễn Thanh Tuấn <ila.thanhtuan@gmail.com>
2024-10-24 19:18:47 +0700

    Add UCS into main.py

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from solver import Solver
 
 if __name__ == "__main__":
     # Parse arguments or input for algorithm choice and file paths
-    algorithm = "UCS"  # Example, use argument parsing to select this dynamically
+    algorithm = "DFS"  # Example, use argument parsing to select this dynamically
     input_file = "inputs/input-01.txt"
     output_file = "outputs/output-01.txt"
 
     solver = Solver(algorithm, input_file, output_file)
     solver.run()
 
-    # algorithm = "DFS"  # Example, use argument parsing to select this dynamically
-    # input_file = "inputs/input-01.txt"
-    # output_file = "outputs/outputDFS-01.txt"
-    # solver = Solver(algorithm, input_file, output_file)
-    # solver.run()
+    algorithm = "BFS"  # Example, use argument parsing to select this dynamically
+    input_file = "inputs/input-01.txt"
+    output_file = "outputs/output-02.txt"
+    solver = Solver(algorithm, input_file, output_file)
+    solver.run()
+
+    algorithm = "UCS"  # Example, use argument parsing to select this dynamically
+    input_file = "inputs/input-01.txt"
+    output_file = "outputs/output-03.txt"
+    solver = Solver(algorithm, input_file, output_file)
+    solver.run()
